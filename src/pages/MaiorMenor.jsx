3d8bbc8efb18789877f7d555454edf6e30497f03
Augusto Akira Miasato <149327671+miyazatoakira@users.noreply.github.com>
2024-05-14 12:20:57 -0300

    Atualizar o MaiorMenor.jsx

--- src/pages/MaiorMenor.jsx
+++ src/pages/MaiorMenor.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Form from '../components/Form';
 
 const MaiorMenor = () => {
-    const [valores, setValores] = useState({ v1: '', v2: '', v3: ''});
-    const [tooltips, setTooltips] = useState({ State1: 'Maior Valor', State2: '', State3: 'Menor Valor' });
+    const [valores, setValores] = useState({ v1: '', v2: '', v3: '' });
+    const [tooltips, setTooltips] = useState({ State1: '', State2: '', State3: '' });
 
     const handleChange = (event) => {
         setValores({ ...valores, [event.target.id]: event.target.value });
@@ -16,32 +16,36 @@ const MaiorMenor = () => {
 
         let maiorValor = valor1;
         let menorValor = valor1;
+        let valorMedio = null;
 
-        let State1 = 'Maior Valor';
-        let State2 = 'Média';
-        let State3 = 'Menor Valor';
+        let State1 = '';
+        let State2 = '';
+        let State3 = '';
 
         if (valor2 > maiorValor) {
             maiorValor = valor2;
-            State1 = '';
-            State2 = 'Maior Valor';
-        }
-        if (valor2 < menorValor) {
+        } else if (valor2 < menorValor) {
             menorValor = valor2;
-            State3 = '';
-            State2 = 'Menor Valor';
         }
 
         if (valor3 > maiorValor) {
             maiorValor = valor3;
-            State1 = '';
-            State2 = '';
-            State3 = 'Maior Valor';
-        }
-        if (valor3 < menorValor) {
+        } else if (valor3 < menorValor) {
             menorValor = valor3;
-            State3 = 'Menor Valor';
         }
+
+        // Verifica o valor médio entre o maior e o menor
+        if ((valor1 < maiorValor && valor1 > menorValor) || (valor1 > maiorValor && valor1 < menorValor)) {
+            valorMedio = valor1;
+        } else if ((valor2 < maiorValor && valor2 > menorValor) || (valor2 > maiorValor && valor2 < menorValor)) {
+            valorMedio = valor2;
+        } else if ((valor3 < maiorValor && valor3 > menorValor) || (valor3 > maiorValor && valor3 < menorValor)) {
+            valorMedio = valor3;
+        }
+
+        State1 = `Maior Valor: ${maiorValor}`;
+        State2 = `Médio Valor: ${valorMedio !== null ? valorMedio : 'Não há valor médio'}`;
+        State3 = `Menor Valor: ${menorValor}`;
 
         setTooltips({ State1, State2, State3 });
     }, [valores]);
@@ -52,7 +56,7 @@ const MaiorMenor = () => {
                 <div className="col-6">
                     <h2 className="h2 fw-bold mt-5">Exercício 3 - JS e React</h2>
                     <div className="line mt-1"></div>
-                    <p>Faça um script que leia três números inteiros, em seguida mostre o maior e o menor deles.</p>
+                    <p>Faça um script que leia três números inteiros, em seguida mostre o maior, o menor e o valor entre eles.</p>
                     <Form type="number" msg="Número 1" id="v1" onChange={handleChange} />
                     <Form type="number" msg="Número 2" id="v2" onChange={handleChange} />
                     <Form type="number" msg="Número 3" id="v3" onChange={handleChange} />
@@ -60,9 +64,9 @@ const MaiorMenor = () => {
                         <div className="card-header">Maior, Média e Menor valores</div>
                         <div className="card-body">
                             <p className="card-text">
-                                Input 1: {tooltips.State1}<br></br>
-                                Input 2: {tooltips.State2}<br></br>
-                                Input 3: {tooltips.State3}
+                                {tooltips.State1}<br />
+                                {tooltips.State2}<br />
+                                {tooltips.State3}
                             </p>
                         </div>
                     </div>
